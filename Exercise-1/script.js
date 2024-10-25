877dd8b248d5886a44ddf54bb35edf2d58d858ef
LSandborgh <lukas.sandborgh@gmail.com>
2024-10-25 09:16:58 +0200

    Creation of Repo

---
+++ Exercise-1/script.js
@@ -0,0 +1,11 @@
+let age = 25;
+let year = 2918
+console.log(age, year);
+
+//Const variable that can't be changed after being initialized;
+const test = 5;
+console.log(test);
+
+//Old way of creating variables
+var score = 75;
+console.log(score);
